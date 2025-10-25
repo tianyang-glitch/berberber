e3ae245f25b87257d50adee5220a54e0999a026f
tianyang-glitch <15722569818@163.com>
2025-10-25 15:20:38 +0800

    试pycharm
    第一次使用github上传文件

---
+++ 333.py
@@ -0,0 +1,14 @@
+# print('hello world')
+# print('what is your name?')
+# name = input()
+# print('it is good to meet you,'+name)
+# print('the length of your name is:')
+# print(len(name))
+# print('what is your age?')
+# age = input()
+# print('you will be'+str(int(age)+1)+'in a year')
+
+num1=57
+num2=200000
+message="tianyang%s,毕业工资%s"%(num1,num2)
+print(message)
